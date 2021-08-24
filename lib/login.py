8ac360677f03fe8d7dcbe42f4ee9a49f75d4c699
Jose Antonio Aviles Pacheco <joseok56@gmail.com>
2021-08-24 01:11:57 -0500

    modification view login

--- lib/login.py
+++ lib/login.py
@@ -6,18 +6,26 @@ import dash_bootstrap_components as dbc
 login_users=html.Div([
     
     dcc.Location(id='login-url',pathname='/login',refresh=False),
+    # view_login
     dbc.Container(
-        children=[dbc.Row(
+        children=[
+            dbc.Row(
+                     dbc.Col(
+                         html.Img(src='https://www.masivocapital.co/images/ImagenesMC/LOGO-MASIVO-01.png',className="imagen_center",
+                                  width="287px",height="192px",),width=6, 
+                     ),justify='center',
+                 ),
+                 dbc.Row(
                     dbc.Col(
                         dbc.Card([
-                             html.H4('Login',className='card-title', style={'color':'#ffff'}),
+                             html.H4('Login',className='card-title muted text-center', style={'color':'black'}),
                              dbc.Input(id='login-email',placeholder='User-App',value='DgoRomr'),
                              dbc.Input(id='login-password',placeholder='Assigned password',type='password', value='adjsfbq5'),
                              dbc.Button('Submit',id='login-button',color='primary',block=True),
                              html.Br(),
                              html.Div(id='login-alert'),
                              ],body=True
-                                ), width=6
+                                ), width=4
                             ),justify='center'
                          ),
                 html.Br(),
@@ -27,22 +35,17 @@ login_users=html.Div([
                                   ),width=6,
                      ),justify='center',
                  ),
-                html.Br(),
-                dbc.Row(
-                     dbc.Col(
-                         html.Img(src="https://www.correlation-one.com/hubfs/c1logo_color.png",className="imagen_center",
-                                  width="566px",height="50px",),width=6,
-                     ), justify='center',
+                # html.Br(),
+                # dbc.Row(
+                #      dbc.Col(
+                #          html.Img(src="https://www.correlation-one.com/hubfs/c1logo_color.png",className="imagen_center",
+                #                   width="566px",height="50px",),width=6,
+                #      ), justify='center',
                      
-                 ),
-                 html.Br(),
-                 dbc.Row(
-                     dbc.Col(
-                         html.Img(src='https://www.masivocapital.co/images/ImagenesMC/LOGO-MASIVO-01.png',className="imagen_center",
-                                  width="287px",height="192px",),width=6, 
-                     ),justify='center',
-                 ),
+                #  ),
+                #  html.Br(),
+                 
                  ]#
             )
-        ]
+        ], className="login"
     )
